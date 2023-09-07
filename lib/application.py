bede40004d5d345bc23e2c80b71850dec7af7e01
Your Name <youremail@example.com>
2023-09-07 21:20:59 +0300

    commit message

--- lib/application.py
+++ lib/application.py
@@ -89,48 +89,6 @@ def all_reviews(restaurant_id):
     return [full_review(review.id) for review in restaurant.reviews]
 
 # # Example usage
-# if __name__ == '__main__':
-#     # Create a new review for a customer
-#     add_review(1, 1, 4)  # Add a review with star rating 4 for Restaurant A by Customer John Doe
-
-#     # Retrieve and print customer's reviews
-#     johns_reviews = reviews(1)  # Get reviews for Customer John Doe
-#     for review in johns_reviews:
-#         print(f"Review for {review.restaurant.name}: {review.star_rating} stars")
-
-#     # Retrieve and print restaurants reviewed by a customer
-#     johns_restaurants = restaurants(1)  # Get restaurants reviewed by Customer John Doe
-#     for restaurant in johns_restaurants:
-#         print(f"{restaurant.name} was reviewed by John Doe")
-
-#     # Retrieve and print a restaurant's reviews
-#     restaurant_a_reviews = all_reviews(1)  # Get all reviews for Restaurant A
-#     for review in restaurant_a_reviews:
-#         print(review)
-
-#     # Find the fanciest restaurant
-#     fanciest_restaurant = Restaurant.fanciest()  # Correct
-#     print(f"The fanciest restaurant is {fanciest_restaurant.name}")
-
-#     # Find John Doe's favorite restaurant
-#     johns_favorite = favorite_restaurant(1)  # Get John Doe's favorite restaurant
-#     print(f"{johns_favorite.name} is John Doe's favorite restaurant")
-
-#     # Delete all of John Doe's reviews for Restaurant A
-#     delete_reviews(1, 1)  # Delete John Doe's reviews for Restaurant A
-
-#     # Confirm the reviews have been deleted
-#     restaurant_a_reviews = all_reviews(1)  # Get all reviews for Restaurant A again
-#     if not restaurant_a_reviews:
-#         print("John Doe's reviews for Restaurant A have been deleted")
-#     else:
-#         print("Deletion failed")
-
-#     # Test the full_review method for a specific review
-#     review_text = full_review(1)  # Get the full review text for a review with ID 1
-#     print(review_text)
-
-
 if __name__ == '__main__':
     # Create a new review for a customer
     add_review(1, 1, 4)  # Add a review with star rating 4 for Restaurant A by Customer John Doe
